f51b9fd629d02966ec252ce4319141dc80bae771
bruno-br <bruno.brasilf@gmail.com>
2024-01-05 12:09:41 -0300

    feat: format table content

--- src/index.js
+++ src/index.js
@@ -2,27 +2,30 @@ import blessed from 'blessed'
 import { Table } from './widgets.js'
 import { getClipboardHistory, updateClipboardHistory } from './clipboard_manager.js'
 
+const prefixLength = 4
+const maxValueLength = 20
+
 const screen = blessed.screen()
 const clipboardTable = Table({
-    keys: true,
-    vi: true,
-    fg: 'white',
-    selectedFg: 'white',
-    selectedBg: 'blue',
-    interactive: true,
-    label: 'Clipboard History',
-    width: '50%',
-    height: '50%',
-    border: { type: "line", fg: "white" },
-    columnSpacing: 1,
-    columnWidth: [16, 12]
+  keys: true,
+  vi: true,
+  fg: 'white',
+  selectedFg: 'white',
+  selectedBg: 'blue',
+  interactive: true,
+  label: 'Clipboard History',
+  width: '50%',
+  height: '50%',
+  border: { type: "line", fg: "white" },
+  columnSpacing: 4,
+  columnWidth: [18, 36]
 })
 
 clipboardTable.focus()
 screen.append(clipboardTable)
 
 clipboardTable.setData({
-  headers: ['Index', 'Content'],
+  headers: [' '.repeat(prefixLength) + 'Titles', 'Values'],
   data: getClipboardTableData()
 })
 
@@ -39,8 +42,15 @@ function getClipboardTableData() {
   const clipboardHistory = getClipboardHistory()
 
   for (let count = 0; count < clipboardHistory.length; count++) {
-    result.push([count, clipboardHistory[count]])
+    const value = formatClipboardValue(clipboardHistory[count])
+    result.push([`[${count}] Clipboard ${count}`, value])
   }
 
   return result
+}
+
+function formatClipboardValue(value) {
+  if (value.length > maxValueLength)
+    value = `${value.slice(0, maxValueLength)}...`
+  return JSON.stringify(value)
 }
